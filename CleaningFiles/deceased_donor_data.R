41307bfaeebaa2e56665eeacae52b8f922b6d043
jbb2xd <67154769+jbb2xd@users.noreply.github.com>
2020-11-09 16:13:41 -0500

    Add files via upload

--- CleaningFiles/deceased_donor_data.R
+++ CleaningFiles/deceased_donor_data.R
@@ -3,27 +3,22 @@ sourcedir <-"~/4th year/SYS 4021/Source"
 
 setwd(traindir)
 donorData <- read.csv(file = 'deceased_donor_data.tsv', sep = '\t')
-donorData <- read.tsv("deceased_donor_data.tsv")
+
 
 summary(donorData)
 colnames(donorData)
 
-#cigarette use
-donorData[which(!is.na(donorData[98])),98]
-Cigs <- rep(NA, nrow(donorData))
-Cigs[which(donorData$HIST_CIG_DON == 'Y')] <- 'Yes'
-Cigs[which(donorData$HIST_CIG_DON == 'N')] <- 'No'
-Cigs[which(donorData$HIST_CIG_DON == 'U')] <- 'Unknown'
-Cigs[which(is.na(donorData$HIST_CIG_DON))] <- 'Unknown'
 
-Cigs
 
-hist(Cigs)
-barplot(table(Cigs), main = "History of Cigarette Usage?", )
 
-donorData[which(!is.na(donorData$CDC_RISK_HIV_DON)), 107]
-donorData1 <- na.omit(donorData)
-max(donorData1$DDR1)
+
+#final Data set with only people under the age of 30 and their heart was recovered
+
+newDonorData <- donorData[which(donorData$AGE_DON <= 30 & donorData$NUM_HR_RECOV == 1),]
+
+
+
+
 
 
 #finding max and min of all columns
@@ -35,5 +30,17 @@ results <- sapply(donorData, mean, na.rm = TRUE)
 install.packages('xlsx')
 library(xlsx)
 results
-#open and close the file after each storage of variable
+#open and close the file after each storage of variable (you need to run this multiple times depending on which variable you want)
 write.xlsx(as.data.frame(results), file = "results.xlsx")
+
+#cigarette use
+Cigs <- rep(NA, nrow(newDonorData))
+Cigs[which(newDonorData$HIST_CIG_DON == 'Y')] <- 'Yes'
+Cigs[which(newDonorData$HIST_CIG_DON == 'N')] <- 'No'
+Cigs[which(newDonorData$HIST_CIG_DON == 'U')] <- 'Unknown'
+Cigs[which(is.na(newDonorData$HIST_CIG_DON))] <- 'Unknown'
+
+Cigs
+
+hist(Cigs)
+barplot(table(Cigs), main = "History of Cigarette Usage?", )
