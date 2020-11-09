3a065a325f28c7673876eb78a9affe62e2e109a9
gmw8hn <73448952+gmw8hn@users.noreply.github.com>
2020-11-09 18:52:56 -0500

    Update thoracic_stat1a_analysis.R
    updated data cleaning

--- thoracic_stat1a_analysis.R
+++ thoracic_stat1a_analysis.R
@@ -8,7 +8,6 @@ names(stat1a)
 stat1a = stat1a[which(stat1a$AGE_GROUP == "Pediatric"), ] 
 
 summary(stat1a)
-summary(stat1a$CHD)
 
 names(stat1a)
 
@@ -23,6 +22,62 @@ print(var[1:15])
 #first 14 variables: all 0 or N/A
 #why are there 14k+ N/As for the first 15 variables? Are variables 2-14 just for adults?
 
+# Recoding first 14 variables (all either 0 or N/A)
+summary(stat1a$MECH_CIRC_ACUTE)
+stat1a$MECH_CIRC_ACUTE[which(stat1a$MECH_CIRC_ACUTE == 0)] <- "0"
+stat1a$MECH_CIRC_ACUTE <- as.factor(stat1a$MECH_CIRC_ACUTE)
+summary(stat1a$MECH_CIRC_ACUTE)
+
+stat1a$MECH_CIRC_ACUTE_VAD[which(stat1a$MECH_CIRC_ACUTE_VAD == 0)] <- "0"
+stat1a$MECH_CIRC_ACUTE_VAD <- as.factor(stat1a$MECH_CIRC_ACUTE_VAD)
+summary(stat1a$MECH_CIRC_ACUTE_VAD)
+
+stat1a$TAH[which(stat1a$TAH == 0)] <- "0"
+stat1a$TAH <- as.factor(stat1a$TAH)
+summary(stat1a$TAH)
+
+stat1a$IAB_PUMP[which(stat1a$IAB_PUMP == 0)] <- "0"
+stat1a$IAB_PUMP <- as.factor(stat1a$IAB_PUMP)
+summary(stat1a$IAB_PUMP)
+
+stat1a$EMO[which(stat1a$EMO == 0)] <- "0"
+stat1a$EMO <- as.factor(stat1a$EMO)
+summary(stat1a$EMO)
+
+stat1a$MECH_CIRC_30DAYS[which(stat1a$MECH_CIRC_30DAYS == 0)] <- "0"
+stat1a$MECH_CIRC_30DAYS <- as.factor(stat1a$MECH_CIRC_30DAYS)
+summary(stat1a$MECH_CIRC_30DAYS)
+
+stat1a$THROMB[which(stat1a$THROMB == 0)] <- "0"
+stat1a$THROMB <- as.factor(stat1a$THROMB)
+summary(stat1a$THROMB)
+
+stat1a$DEVICE_INFECT[which(stat1a$DEVICE_INFECT == 0)] <- "0"
+stat1a$DEVICE_INFECT <- as.factor(stat1a$DEVICE_INFECT)
+summary(stat1a$DEVICE_INFECT)
+
+stat1a$DEV_MALFUNCT[which(stat1a$DEV_MALFUNCT == 0)] <- "0"
+stat1a$DEV_MALFUNCT <- as.factor(stat1a$DEV_MALFUNCT)
+summary(stat1a$DEV_MALFUNCT)
+
+stat1a$LIFE_THREAT_VENT_ARRHY[which(stat1a$LIFE_THREAT_VENT_ARRHY == 0)] <- "0"
+stat1a$LIFE_THREAT_VENT_ARRHY <- as.factor(stat1a$LIFE_THREAT_VENT_ARRHY)
+summary(stat1a$LIFE_THREAT_VENT_ARRHY)
+
+stat1a$OTHER_DEV_THERAPY[which(stat1a$OTHER_DEV_THERAPY == 0)] <- "0"
+stat1a$OTHER_DEV_THERAPY <- as.factor(stat1a$OTHER_DEV_THERAPY)
+summary(stat1a$OTHER_DEV_THERAPY)
+
+stat1a$MECH_VENT[which(stat1a$MECH_VENT == 0)] <- "0"
+stat1a$MECH_VENT <- as.factor(stat1a$MECH_VENT)
+summary(stat1a$MECH_VENT)
+
+stat1a$CONT_INFUS[which(stat1a$CONT_INFUS == 0)] <- "0"
+stat1a$CONT_INFUS <- as.factor(stat1a$CONT_INFUS)
+summary(stat1a$CONT_INFUS)
+
+
+## Barplots of vars 15 - 26
 par(mfrow=c(3,4))
 for(i in 15:26) {
   barplot(table(stat1a[, var[i]]), main=var[i])
@@ -30,15 +85,75 @@ for(i in 15:26) {
 }
 print(var[15:26])
 
+
+# Recoding variables 15 - 26 (all 0,1, or NA)
+stat1a$EXCEPTION[which(stat1a$EXCEPTION == 0)] <- "0"
+stat1a$EXCEPTION[which(stat1a$EXCEPTION == 1)] <- "1"
+stat1a$EXCEPTION <- as.factor(stat1a$EXCEPTION)
+summary(stat1a$EXCEPTION)
+
+stat1a$REQ_VENTILATOR[which(stat1a$REQ_VENTILATOR == 0)] <- "0"
+stat1a$REQ_VENTILATOR[which(stat1a$REQ_VENTILATOR == 1)] <- "1"
+stat1a$REQ_VENTILATOR <- as.factor(stat1a$REQ_VENTILATOR)
+summary(stat1a$REQ_VENTILATOR)
+
+stat1a$REQ_MECH_ASSIST_DEV[which(stat1a$REQ_MECH_ASSIST_DEV == 0)] <- "0"
+stat1a$REQ_MECH_ASSIST_DEV[which(stat1a$REQ_MECH_ASSIST_DEV == 1)] <- "1"
+stat1a$REQ_MECH_ASSIST_DEV <- as.factor(stat1a$REQ_MECH_ASSIST_DEV)
+summary(stat1a$REQ_MECH_ASSIST_DEV)
+
+stat1a$REQ_BALLON_PUMP[which(stat1a$REQ_BALLON_PUMP == 0)] <- "0"
+stat1a$REQ_BALLON_PUMP[which(stat1a$REQ_BALLON_PUMP == 1)] <- "1"
+stat1a$REQ_BALLON_PUMP <- as.factor(stat1a$REQ_BALLON_PUMP)
+summary(stat1a$REQ_BALLON_PUMP)
+
+stat1a$LESS_6M_HRT_DISEASE[which(stat1a$LESS_6M_HRT_DISEASE == 0)] <- "0"
+stat1a$LESS_6M_HRT_DISEASE[which(stat1a$LESS_6M_HRT_DISEASE == 1)] <- "1"
+stat1a$LESS_6M_HRT_DISEASE <- as.factor(stat1a$LESS_6M_HRT_DISEASE)
+summary(stat1a$LESS_6M_HRT_DISEASE)
+
+stat1a$REQ_INF_INOTROPE[which(stat1a$REQ_INF_INOTROPE == 0)] <- "0"
+stat1a$REQ_INF_INOTROPE[which(stat1a$REQ_INF_INOTROPE == 1)] <- "1"
+stat1a$REQ_INF_INOTROPE <- as.factor(stat1a$REQ_INF_INOTROPE)
+summary(stat1a$REQ_INF_INOTROPE)
+
+stat1a$REQ1_CONTINUOUS_VENTILATION[which(stat1a$REQ1_CONTINUOUS_VENTILATION == 0)] <- "0"
+stat1a$REQ1_CONTINUOUS_VENTILATION[which(stat1a$REQ1_CONTINUOUS_VENTILATION == 1)] <- "1"
+stat1a$REQ1_CONTINUOUS_VENTILATION <- as.factor(stat1a$REQ1_CONTINUOUS_VENTILATION)
+summary(stat1a$REQ1_CONTINUOUS_VENTILATION)
+
+stat1a$REQ2_BALLOONPUMP[which(stat1a$REQ2_BALLOONPUMP == 0)] <- "0"
+stat1a$REQ2_BALLOONPUMP[which(stat1a$REQ2_BALLOONPUMP == 1)] <- "1"
+stat1a$REQ2_BALLOONPUMP <- as.factor(stat1a$REQ2_BALLOONPUMP)
+summary(stat1a$REQ2_BALLOONPUMP)
+
+stat1a$REQ3_DUCTAL_DEPENDENCY[which(stat1a$REQ3_DUCTAL_DEPENDENCY == 0)] <- "0"
+stat1a$REQ3_DUCTAL_DEPENDENCY[which(stat1a$REQ3_DUCTAL_DEPENDENCY == 1)] <- "1"
+stat1a$REQ3_DUCTAL_DEPENDENCY <- as.factor(stat1a$REQ3_DUCTAL_DEPENDENCY)
+summary(stat1a$REQ3_DUCTAL_DEPENDENCY)
+
+stat1a$REQ4_CHD_DIAGNOSIS[which(stat1a$REQ4_CHD_DIAGNOSIS == 0)] <- "0"
+stat1a$REQ4_CHD_DIAGNOSIS[which(stat1a$REQ4_CHD_DIAGNOSIS == 1)] <- "1"
+stat1a$REQ4_CHD_DIAGNOSIS <- as.factor(stat1a$REQ4_CHD_DIAGNOSIS)
+summary(stat1a$REQ4_CHD_DIAGNOSIS)
+
+stat1a$REQ5_CIRCULATORY_DEVICE[which(stat1a$REQ5_CIRCULATORY_DEVICE == 0)] <- "0"
+stat1a$REQ5_CIRCULATORY_DEVICE[which(stat1a$REQ5_CIRCULATORY_DEVICE == 1)] <- "1"
+stat1a$REQ5_CIRCULATORY_DEVICE <- as.factor(stat1a$REQ5_CIRCULATORY_DEVICE)
+summary(stat1a$REQ5_CIRCULATORY_DEVICE)
+
+stat1a$REQ6_CRITERIA_NOT_MET[which(stat1a$REQ6_CRITERIA_NOT_MET == 0)] <- "0"
+stat1a$REQ6_CRITERIA_NOT_MET[which(stat1a$REQ6_CRITERIA_NOT_MET == 1)] <- "1"
+stat1a$REQ6_CRITERIA_NOT_MET <- as.factor(stat1a$REQ6_CRITERIA_NOT_MET)
+summary(stat1a$REQ6_CRITERIA_NOT_MET)
+
+
 # look at other statistics
-stat1a$REQ_INF_INOTROPE <- factor(stat1a$REQ_INF_INOTROPE, labels = c("0", "1"))
 summary(stat1a$REQ_INF_INOTROPE)
 #40.47% require infusion of a single high-dose intravenous inotrope
 
-stat1a$REQ_MECH_ASSIST_DEV <- factor(stat1a$REQ_MECH_ASSIST_DEV, labels = c("0", "1"))
 summary(stat1a$REQ_MECH_ASSIST_DEV)
 #15.9% require assistance with a mechanical assist device (Ped B)
-
 
 length(table(stat1a$WL_ID_CODE))
 #9425 unique patients 
@@ -51,13 +166,6 @@ barplot(table(stat1a$CHD), main="CHD")
 length(table(stat1a$CHD))
 table(stat1a$CHD)
 #135 different CHD codes
-
-high_chd = table(stat1a$CHD)[which(table(stat1a$CHD) > 100)]
-high_chd
-## 8232 with no CHD
-# 2079 with code 2 -
-# 352 with code 2048
-# Codes are not the best indicator of what's going on, comments might be better?
 
 
 table(stat1a$CHD_OTHER_SPECIFY)
@@ -156,9 +264,6 @@ for(i in 1:6809) {
 # ~2500 patients that have multiple timestamps had changes in values
 # some of these changes might be attributed to coding of 0 vs. N/A in possible adult columns so this value might be smaller later
 
-#next steps: look at specific variables that have large differences between patients
-# inotropes, CHD, etc.
-
-
+summary(stat1a)
 
 
