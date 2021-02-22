59a5c1b54e8ba4713b0a92cbb363edf30602e113
jbb2xd <67154769+jbb2xd@users.noreply.github.com>
2021-02-22 01:02:01 -0500

    Add files via upload

---
+++ John_duration_calculated.R
@@ -0,0 +1,26 @@
+data_dir = "~/4th year/STS Capstone/"
+setwd(data_dir)
+
+library("readxl")
+library(ggplot2)
+library(tidyverse)
+
+#used after sorting echo to top of data
+data = read_csv("full7100wEcho.csv")
+data$DT = as.POSIXct(data$DT, format = "%m/%d/%Y %H:%M" )
+data$BrainDeath = as.POSIXct(data$BrainDeath, format = "%m/%d/%Y %H:%M" )
+
+#up to here is how to read stuff 
+
+#Add duration column and calculate for each unique donor
+data$Duration = rep(NA, nrow(data))
+data$Duration = data$DT - data$BrainDeath
+#data = data %>% group_by(DONOR_ID) %>% mutate(Duration = DT - BrainDeath)
+#convert duration from seconds to hours
+data$Duration = data$Duration / 3600
+data$Duration = as.numeric(data$Duration)
+
+#write to csv file
+write.csv(data, "~/4th year/STS Capstone/full7100wEchoduration.csv", row.names = FALSE)
+
+
