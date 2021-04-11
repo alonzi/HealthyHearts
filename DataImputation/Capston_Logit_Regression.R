6477640b3d515de6ce88031744007ef31d891ebd
ianpedersen2 <73144204+ianpedersen2@users.noreply.github.com>
2021-04-11 18:06:53 -0400

    Add files via upload

---
+++ DataImputation/Capston_Logit_Regression.R
@@ -0,0 +1,230 @@
+# https://www.hackerearth.com/practice/machine-learning/machine-learning-algorithms/logistic-regression-analysis-r/tutorial/
+# https://online.datasciencedojo.com/blog/logistic-regression-in-r-a-classification-technique-to-predict-credit-card-default
+# https://rpubs.com/abhaypadda/logistic-regression-using-titanic-data
+
+## Load the necessary packages
+library(dplyr)
+library(dummies)
+library(caTools)
+library(MASS)
+library(zoo)
+
+
+## Load the data and delete the unuseful columns
+data_dir <- "C:/Users/ianpe/Documents/UVA/Fourth Year/Capstone"
+setwd(data_dir)
+df <- read.csv("imputed_7100.csv")
+
+#loading functions to create dataframes at different discrete times
+mrm_donor_time <- function(df, donor_id, time) {
+  
+  donor_info <- df[which(df$DONOR_ID == donor_id), ]
+  donor_duration <- donor_info[which(donor_info$Duration <= time), ]
+  if(((max(donor_duration$Duration) <= time) == TRUE) & (is.infinite(max(donor_duration$Duration)) == FALSE)){
+    
+    #checks if there is a measurement at or before that time
+    donor_duration <- donor_duration[order(donor_duration[,c("Duration")],decreasing = TRUE),]
+    #getting duration value of the measurement equal to or closest to specified duration
+    #next observation carried forward
+    if(nrow(donor_duration) > 1){ #checks for multiple observations 
+      donor_duration <- donor_duration %>% mutate(donor_duration,na.locf(donor_duration,fromLast = TRUE, na.rm = FALSE))
+    }
+    else{
+      donor_duration <- donor_duration
+    }
+    most_recent <- donor_duration[which.max(donor_duration$Duration), ]
+    return(most_recent)
+  }
+  else{
+    temp_row <- donor_info[1,] #temporary row that will be removed in a later function that calls this one
+    temp_row <- rep(NA)
+    return(temp_row)
+  }
+}
+
+mrm_df_time <- function(df,time){
+  uni_dons <- unique(df$DONOR_ID)
+  data_mrm <- mrm_donor_time(df,uni_dons[1],time)
+  
+  for(i in 2:(length(uni_dons))){
+    temp_vec <- mrm_donor_time(df, uni_dons[i], time)
+    data_mrm <- rbind(data_mrm,temp_vec)
+  }
+  data_mrm <- data_mrm[rowSums(is.na(data_mrm)) != ncol(data_mrm),] #removes empty rows
+  return(data_mrm)
+}
+
+#data frame of most recent measurements at 24 hours after braindeath
+df.24dur <- mrm_df_time(df,24) 
+
+### going to use 24 hours as the test df
+drop_col <- c(1:4,6:8,18)
+#df <- df %>% select(-c(DONOR_ID,DT,DONOR_ID,BrainDeath,Vasopressin,TRR_ID_CODE,DOB_DON,DOB,ï..,DON_ID))
+df.24dur <- df.24dur[,-drop_col] #the line above is buggy but those are the dropped columns
+#vasopressin is dropped because the distribution is skewed
+
+## Check data structure
+str(df.24dur)
+
+## Let's check for any missing values in the data
+colSums(is.na(df.24dur))
+
+## Checking for empty values
+colSums(df.24dur =='')
+
+## Standardizaiton for the numerical data
+df.24dur <- df.24dur %>%
+  mutate_at(c(2:17), funs(c(scale(.))))
+
+## Check number of uniques values for each of the column to find out columns which we can convert to factors
+sapply(df.24dur, function(x) length(unique(x)))
+
+as.numeric(unlist(df.24dur))
+
+str(df.24dur)
+
+## Converting the categorical data to factors
+for (i in c("Mitral.valve.insufficiency","Aortic.valve.insufficiency","Tricuspid.valve.insufficiency",
+            "Pulmonary.valve.insufficiency","Global.Left.Ventricular.Dysfunction","Global.Right.Ventricular.Dysfunction",
+            "Qualitative_Status","Quantitative_Status","age.group")){
+  df.24dur[,i]=as.factor(df.24dur[,i])
+}
+
+## Create dummy variables for categorical variables
+str(df.24dur)
+names(df.24dur)
+df.24dur <- dummy.data.frame(df.24dur, names=c("Mitral.valve.insufficiency","Aortic.valve.insufficiency","Tricuspid.valve.insufficiency",
+                                           "Pulmonary.valve.insufficiency","Global.Left.Ventricular.Dysfunction","Global.Right.Ventricular.Dysfunction",
+                                           "Qualitative_Status","Quantitative_Status","age.group"), sep="_")
+
+
+## Some features need to be deleted after hot-encoding due to singularity.
+df.24dur = subset(df.24dur, select = -c(Mitral.valve.insufficiency_2,Aortic.valve.insufficiency_1,Tricuspid.valve.insufficiency_2,
+                                    Pulmonary.valve.insufficiency_1,Global.Left.Ventricular.Dysfunction_3,Global.Right.Ventricular.Dysfunction_1,
+                                    Qualitative_Status_2,Quantitative_Status_2,age.group_6))
+
+
+
+
+#make separate dataframes so regression model is at different discrete duration times
+#times are chosen based on sample size, so 6 hours is ~100 observations
+
+
+
+## Splitting training and test data for 6 hours
+
+summary(df.24dur)
+str(df.24dur)
+set.seed(100) 
+
+sample <- sample.int(n = nrow(df.24dur), size = floor(.80*nrow(df.24dur)), replace = F)
+train <- df.24dur[sample, ]
+test  <- df.24dur[-sample, ]
+
+train
+
+
+## Model Creation
+model <- glm(Accepted ~ Age + Duration + Weight + Height + Global.Left.Ventricular.Dysfunction_0 +
+               Global.Left.Ventricular.Dysfunction_1 + Global.Left.Ventricular.Dysfunction_2 +
+               Qualitative_Status_0 + Quantitative_Status_0 + Qualitative_Status_1 + 
+               Quantitative_Status_1 + Score + X4.Chamber.Ejection.Fraction.. + 
+               Shortening.Fraction..,family=binomial(link='logit'),data=train,maxit = 200)
+
+## Model Summary
+summary(model)
+
+## Using anova() to analyze the table of deviance
+anova(model, test="Chisq")
+
+## Predicting Test Data 
+result <- predict(model,newdata=test,type='response')
+result <- ifelse(result > 0.5,1,0)
+
+result
+
+## Confusion matrix
+library(caret)
+accepted <- as.factor(test$Accepted)
+result <- as.factor(result)
+confusionMatrix(data=result, reference=as.factor(test$Accepted))
+
+sum(test$Accepted)
+
+## ROC Curve and calculating the area under the curve(AUC)
+library(ROCR)
+predictions <- predict(model, newdata=df.24dur, type="response")
+ROCRpred <- prediction(predictions, df.24dur$Accepted)
+ROCRperf <- performance(ROCRpred, measure = "tpr", x.measure = "fpr")
+
+plot(ROCRperf, colorize = TRUE, text.adj = c(-0.2,1.7), print.cutoffs.at = seq(0,1,0.1))
+
+auc <- performance(ROCRpred, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+
+## correlation matrix and visualizations
+
+## plot 1
+library(Hmisc)
+library(PerformanceAnalytics)
+
+res <- cor(df.24dur)
+round(res, 2)
+
+## plot 2
+library(corrplot)
+corrplot(res, type = "upper", order = "hclust", 
+         tl.col = "black", tl.srt = 45)
+
+col<- colorRampPalette(c("blue", "white", "red"))(100)
+heatmap(x = res, col = col, symm = TRUE)
+
+## plot 3
+library(heatmaply)
+
+heatmaply_cor(
+  cor(df.24dur),
+  xlab = "Features", 
+  ylab = "Features",
+  k_col = 2, 
+  k_row = 2
+)
+
+## plot 4
+library(tidyverse)
+
+cormat <- round(cor(df.24dur),2)
+library(reshape2)
+melted_cormat <- melt(cormat)
+head(melted_cormat)
+
+lower_tri <- res
+lower_tri[lower.tri(lower_tri)] <- NA #OR upper.tri function
+lower_tri
+corr_res <- reshape2::melt(lower_tri, na.rm = TRUE)
+
+# Heatmap
+ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))+
+  coord_fixed() +
+  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.justification = c(1, 0),
+    legend.position = c(1, 0.7),
+    legend.direction = "horizontal")+
+  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                               title.position = "bottom", title.hjust = 0.5,legend.box = "horizontal"))
+
---
+++ DataImputation/Capstone_Imputation.R
@@ -0,0 +1,300 @@
+# import necessary packages 
+library(dplyr)
+library(ggplot2)
+library(tidyr) 
+library(zoo)
+
+# import data and make it a dataframe
+data_dir <- "C:/Users/ianpe/Documents/UVA/Fourth Year/Capstone"
+setwd(data_dir)
+data7100 <- read.csv("EchoMatchesw7100.csv")
+df_new <- as.data.frame(data7100)
+#add swartz and meld score
+df_new$Schwartz <- rep(NA, nrow(df_new))
+df_new$MELD <- rep(NA, nrow(df_new))
+
+#calculating schwartz score
+schwartz_calc <- function(df){
+  df$Schwartz <- (0.413*df$Height) / df$CREATININE
+}
+df_new$Schwartz <- schwartz_calc(df_new)
+
+#calcluating MELD score
+meld_calc <- function(df){
+  df$MELD <- 3.78*log(df$BILIRUBIN) + 11.2*log(df$INR) + 9.57*log(df$CREATININE) + 6.43
+}
+df_new$MELD <- meld_calc(df_new)
+
+show(df_new)
+
+# drop the useless features for future analysis
+
+drop_col <- c("X61",
+              "X62",
+              "Complete.",
+              "Comment.",
+              "Repeat.Instrument",
+              "Repeat.Instance")
+df_new <- df_new[ , !(names(df_new) %in% drop_col)]
+
+
+
+# age calculation
+
+echo_age <- function(df){
+  #cuts duration to 3 days before brain death
+  df_new.age <- as.data.frame(df_new[which(df_new$Duration>=-72),])
+  df_new.age$Age <- rep(NA, nrow(df_new.age))
+  df_new.age$BrainDeathDate <- rep(NA, nrow(df_new.age))
+  df_new.age$BrainDeathDate <- strptime(df_new.age$BrainDeath, "%m/%d/%Y")
+  df_new.age$DOB_DON <- strptime(df_new.age$DOB_DON, "%m/%d/%Y")
+  df_new.age$Age <- difftime(df_new.age$BrainDeathDate, df_new.age$DOB_DON, units = "weeks") #here is years actually
+  #convert Age from weeks to years
+  df_new.age$Age = df_new.age$Age / 52
+  #Create age groups
+  df_new.age$age.group <- rep(NA, nrow(df_new.age))
+  df_new.age <- mutate(df_new.age, age.group = case_when(
+    df_new.age$Age < 1 ~ "0-1",
+    df_new.age$Age < 3 & df_new.age$Age >= 1 ~ "1-3",
+    df_new.age$Age < 6 & df_new.age$Age >= 3 ~ "3-6",
+    df_new.age$Age < 12 & df_new.age$Age >= 6 ~ "6-12",
+    df_new.age$Age >= 12 ~ "12+",
+    is.na(df_new.age$DOB_DON) == TRUE ~ "" #This is because the glm for age.groups won't work if there are NAs so I assigned this to missing DOB_DON
+  ))
+  
+  df_new.age <-mutate(df_new.age, age.group = case_when(
+    df_new.age$Age < 0.5 ~ "0-0.5",
+    df_new.age$Age < 1 & df_new.age$Age >= 0.5 ~ "0.5-1",
+    TRUE ~ age.group
+  ))
+  as.factor(df_new.age$age.group)
+  df_new.age <- df_new.age[,!(names(df_new.age) %in% c("BrainDeathDate","Repeat.Instrument","Comment.","Complete."))]
+  #df_new.age <- as.character(df_new.age$DOB_DON)
+  return(df_new.age)
+}
+df_new <- echo_age(df_new)
+#needed to change to character bc of bugs, but I am not sure why this fixes it
+df_new$DOB_DON <- as.character(df_new$DOB_DON) 
+
+# drop the values NA's in the "Accepted" column
+df_new <- df_new %>% drop_na(Accepted)
+
+
+#returns a vector of the most recent measurements for that donor at that time
+mrm_donor_time <- function(df, donor_id, time) {
+  
+  donor_info <- df[which(df$DONOR_ID == donor_id), ]
+  donor_duration <- donor_info[which(donor_info$Duration <= time), ]
+  if(((max(donor_duration$Duration) <= time) == TRUE) & (is.infinite(max(donor_duration$Duration)) == FALSE)){
+    
+    #checks if there is a measurement at or before that time
+    donor_duration <- donor_duration[order(donor_duration[,c("Duration")],decreasing = TRUE),]
+    #getting duration value of the measurement equal to or closest to specified duration
+    #next observation carried forward
+    if(nrow(donor_duration) > 1){ #checks for multiple observations 
+      donor_duration <- donor_duration %>% mutate(donor_duration,na.locf(donor_duration,fromLast = TRUE, na.rm = FALSE))
+    }
+    else{
+      donor_duration <- donor_duration
+    }
+    most_recent <- donor_duration[which.max(donor_duration$Duration), ]
+    return(most_recent)
+  }
+  else{
+    temp_row <- donor_info[1,] #temporary row that will be removed in a later function that calls this one
+    temp_row <- rep(NA)
+    return(temp_row)
+  }
+}
+
+#function that gives a data frame of all most recent measurements at certain times
+mrm_df_time <- function(df,time){
+  uni_dons <- unique(df$DONOR_ID)
+  data_mrm <- mrm_donor_time(df,uni_dons[1],time)
+  
+  for(i in 2:(length(uni_dons))){
+    temp_vec <- mrm_donor_time(df, uni_dons[i], time)
+    data_mrm <- rbind(data_mrm,temp_vec)
+  }
+  data_mrm <- data_mrm[rowSums(is.na(data_mrm)) != ncol(data_mrm),] #removes empty rows
+  return(data_mrm)
+}
+
+#this function creates a data frame of the most recent measurements as discrete times
+# it differs from the others because it will include the measurements from previous time,
+# but it is still the most recent at those discrete times
+mrm_df <- function(df){
+  times <- c(0,3,6,12,18,24,36,48,72)
+  df_all <- data.frame()
+  for(i in 1:length(times)){
+    df_temp <- mrm_df_time(df,times[i])
+    df_all <- rbind(df_all,df_temp)
+  }
+  df_all <- df_all[!duplicated(df_all),]
+  return(df_all)
+}
+
+df_new_mrm <- mrm_df(df_new)
+
+#function that calculates the percentage of missing data for each variable
+var_missing <- function(df){
+  if(is.data.frame(df) == TRUE){
+    num_vars <- ncol(df) #number of columns
+    obs <- nrow(df) #number of observations
+    vars <- matrix(data = NA, nrow = 1, ncol = num_vars)
+    colnames(vars) <- names(df)
+    for(i in 1:num_vars){
+      na_count <- sum(is.na(df[[i]])) #how many nas are in that column
+      vars[1,i] <- na_count/obs #gives the percentage of the amount of nas
+    }
+    return(vars)
+  }
+  else{
+    stop("Input is not a data frame")
+  }
+}
+
+var_missing_dur <- function(df){
+  df.0dur <- df[which(df$Duration <= 0),]
+  df.3dur <- df[which(df$Duration <= 3),]
+  df.6dur <- df[which(df$Duration <= 6),]
+  df.12dur <- df[which(df$Duration <= 12),]
+  df.18dur <- df[which(df$Duration <= 18),]
+  df.24dur <- df[which(df$Duration <= 24),]
+  df.36dur <- df[which(df$Duration <= 36),]
+  df.48dur <- df[which(df$Duration <= 48),]
+  df.72dur <- df[which(df$Duration <= 72),]
+  
+  list_DF <- list(df.0dur,df.3dur,df.6dur,df.12dur,df.18dur,df.24dur,df.36dur,df.48dur,df.72dur)
+  varmat_dur <- matrix(data = NA, nrow = (length(list_DF)), ncol = ncol(df))
+  for(i in 1:length(list_DF)){
+    varmat_dur[i,] <- var_missing(list_DF[[i]]) #creates matrix of % var missing by age group
+  }
+  tot_missing <- var_missing(df)
+  tot_var <- rbind(varmat_dur,tot_missing)  #combines duration matrix with total % missing
+  rownames(tot_var) <- c("Time <= 0 hours","Time <= 3 hours","Time <= 6 hours","Time <= 12 hours","Time <= 18 hours","Time <= 24 hours","Time <= 36 hours","Time <= 48 hours","Time <= 72 hours","Total Missing")
+  return(tot_var)
+  
+}
+
+missing_df_mrm <- var_missing_dur(df_new_mrm)
+
+# find out the NA ratios
+
+missing.values <- df_new_mrm %>%
+  gather(key = "key", value = "val") %>%
+  mutate(isna = is.na(val)) %>%
+  group_by(key) %>%
+  mutate(total = n()) %>%
+  group_by(key, total, isna) %>%
+  summarise(num.isna = n()) %>%
+  mutate(pct = num.isna / total * 100)
+
+## `summarise()` regrouping output by 'key', 'total' (override with `.groups` argument)
+levels <-
+  (missing.values  %>% filter(isna == T) %>% arrange(desc(pct)))$key
+
+percentage.plot <- missing.values %>%
+  ggplot() +
+  geom_bar(aes(x = reorder(key, desc(pct)), 
+               y = pct, fill=isna), 
+           stat = 'identity', alpha=0.8) +
+  scale_x_discrete(limits = levels) +
+  scale_fill_manual(name = "", 
+                    values = c('steelblue', 'tomato3'), labels = c("Present", "Missing")) +
+  coord_flip() +
+  labs(title = "Percentage of Missing Values", x =
+         'Features', y = "% of Missing Values")
+
+percentage.plot
+
+
+
+
+# # remove columns with more than 75% NA ratio -> only 14 variables are left
+#Theta_75 = 0.75
+#df_new = df_new[,!sapply(df_new, function(x) mean(is.na(x))) > Theta_75]
+
+#decided t0 change it to 0.8 because score has a little over 75% missing and I wanted to include it
+# 35 variables left instead on 14
+Theta = 0.8
+df_new_mrm = df_new_mrm[,!sapply(df_new_mrm, function(x) mean(is.na(x))) > Theta]
+
+
+# convert non-numerical values to numerical
+df_new_mrm$Accepted <- ifelse(df_new_mrm$Accepted=="Yes",1,0)
+df_new_mrm$Mitral.valve.insufficiency <- substring(df_new_mrm$Mitral.valve.insufficiency, nchar(as.character(df_new_mrm$Mitral.valve.insufficiency)) - 0)
+df_new_mrm$Aortic.valve.insufficiency <- substring(df_new_mrm$Aortic.valve.insufficiency, nchar(as.character(df_new_mrm$Aortic.valve.insufficiency)) - 0)
+df_new_mrm$Tricuspid.valve.insufficiency <- substring(df_new_mrm$Tricuspid.valve.insufficiency, nchar(as.character(df_new_mrm$Tricuspid.valve.insufficiency)) - 0)
+df_new_mrm$Pulmonary.valve.insufficiency <- substring(df_new_mrm$Pulmonary.valve.insufficiency, nchar(as.character(df_new_mrm$Pulmonary.valve.insufficiency)) - 0)
+df_new_mrm$Global.Right.Ventricular.Dysfunction <- substring(df_new_mrm$Global.Right.Ventricular.Dysfunction, nchar(as.character(df_new_mrm$Global.Right.Ventricular.Dysfunction)) - 0)
+df_new_mrm$Global.Left.Ventricular.Dysfunction <- substring(df_new_mrm$Global.Left.Ventricular.Dysfunction, nchar(as.character(df_new_mrm$Global.Left.Ventricular.Dysfunction)) - 0)
+
+summary(df_new_mrm)
+
+write.csv(df_new_mrm,"C:/Users/ianpe/Documents/UVA/Fourth Year/Capstone/7100_cleaned.csv", row.names = FALSE)
+
+str(df_new_mrm)
+# split the full df_new df to based on their data type -> categorical and numerical
+df_new_mrm <- read.csv("C:/Users/ianpe/Documents/UVA/Fourth Year/Capstone/7100_cleaned.csv") # the df_new has to be re-read due to an unknown error
+
+
+# reorder the columns so that cate and num can be perfectly split
+# removed milosprine because it only has NAs
+df_new_mrm <- df_new_mrm[,c(1,2,3,4,5,6,7,8,
+                    9,10,11,12,13,14,15,16,17,19,20,21,22,23,24,25,34,
+                    26,27,28,29,30,31,32,33,35)]
+                    
+names(df_new_mrm)
+
+df_num <- as.data.frame(df_new_mrm[9:25])
+summary(df_num)
+
+df_cate <- as.data.frame(df_new_mrm[26:34])
+
+df_cate$age.group <- as.character(df_cate$age.group)
+df_cate$age.group[which(df_cate$age.group=="0-0.5")] <- "1"
+df_cate$age.group[which(df_cate$age.group=="0.5-1")] <- "2"
+df_cate$age.group[which(df_cate$age.group=="1-3")] <- "3"
+df_cate$age.group[which(df_cate$age.group=="3-6")] <- "4"
+df_cate$age.group[which(df_cate$age.group=="6-12")] <- "5"
+df_cate$age.group[which(df_cate$age.group=="12+")] <- "6"
+df_cate$age.group <- as.numeric(df_cate$age.group)
+
+summary(df_cate)
+
+df_cate
+# df_donor_info and other info
+df_donor_info <- as.data.frame(df_new_mrm[1:8])
+
+# imputation by using the mice package
+library(mice)
+library(VIM)
+library(lattice)
+
+# https://stats.idre.ucla.edu/r/faq/how-do-i-perform-multiple-imputation-using-predictive-mean-matching-in-r/
+
+
+methods(mice)
+
+# impute for numerical value
+imp_num <- mice(df_num, meth="pmm", m = 10)
+imp_num$meth
+completedData_num <- complete(imp_num,1)
+densityplot(imp_num)
+
+
+# impute for categorical value
+imp_cate <- mice(df_cate, meth="pmm", m = 10)
+imp_cate$meth
+completedData_cate <- complete(imp_cate,1)
+densityplot(imp_cate)
+
+# join the datasets
+basic_info <- cbind(df_donor_info)
+echo_data <- cbind(completedData_num,completedData_cate)
+imputed_7100 <- cbind(basic_info,echo_data)
+
+write.csv(imputed_7100,"C:/Users/ianpe/Documents/UVA/Fourth Year/Capstone/imputed_7100.csv", row.names = FALSE)
+
+
